d0bfad34e2e14f1f1283ef4356d354bb2507d640
rydahn <159046885+rydahn@users.noreply.github.com>
2024-02-05 08:52:39 -0800

    Updated cis129_lab02_shortStory.py to include a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+# Author: Jesus Ramos
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("Oh, how the priest prayed!")
+print("On his knees, hands clapsed, for the poor, the sick, the wounded, the frail, the ugly, the forgotten, the plain old unlucky.")
+print("Oh, and for his favorite sports team - he musn't forget them!")
